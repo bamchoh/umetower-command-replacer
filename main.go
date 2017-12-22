76cd1237e96711bde1a36c205e3719e6eef11f0d
bamchoh <bamchoh@gmail.com>
2017-12-23 03:25:23 +0900

    Support config file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/url"
@@ -11,7 +12,92 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var (
+	DefaultUpKey    = "k"
+	DefaultDownKey  = "j"
+	DefaultLeftKey  = "h"
+	DefaultRightKey = "l"
+	DefaultBlockKey = " "
+)
+
+type config struct {
+	Down  string `json:"down"`
+	Up    string `json:"up"`
+	Left  string `json:"left"`
+	Right string `json:"right"`
+	Block string `json:"block"`
+}
+
+func getRuneKey(key string, defaultKey string) rune {
+	if len(key) == 0 {
+		key = defaultKey
+	}
+
+	fmt.Println(key)
+
+	return rune(key[0])
+}
+
+func setUp(mapper map[rune]rune, key string) {
+	runekey := getRuneKey(key, DefaultUpKey)
+	mapper[runekey] = '8'
+}
+
+func setDown(mapper map[rune]rune, key string) {
+	runekey := getRuneKey(key, DefaultDownKey)
+	mapper[runekey] = '2'
+}
+
+func setLeft(mapper map[rune]rune, key string) {
+	runekey := getRuneKey(key, DefaultLeftKey)
+	mapper[runekey] = '4'
+}
+
+func setRight(mapper map[rune]rune, key string) {
+	runekey := getRuneKey(key, DefaultRightKey)
+	mapper[runekey] = '6'
+}
+
+func setBlock(mapper map[rune]rune, key string) {
+	runekey := getRuneKey(key, DefaultBlockKey)
+	mapper[runekey] = '5'
+}
+
+func exists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
+}
+
+func getConfig(filename string) config {
+	var config config
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	dec := json.NewDecoder(f)
+	err = dec.Decode(&config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return config
+}
+
 func main() {
+	configfile := "config.json"
+	var config config
+	if exists(configfile) {
+		config = getConfig(configfile)
+	}
+
+	mapper := make(map[rune]rune, 0)
+	setUp(mapper, config.Up)
+	setDown(mapper, config.Down)
+	setLeft(mapper, config.Left)
+	setRight(mapper, config.Right)
+	setBlock(mapper, config.Block)
+
 	rawurl := ""
 	id := ""
 	if len(os.Args) > 2 {
@@ -34,19 +120,16 @@ func main() {
 		comment := scanner.Text()
 		command := true
 		for _, c := range comment {
-			if c == 'h' || c == 'j' || c == 'k' || c == 'l' || c == ' ' {
-				continue
+			if _, ok := mapper[c]; ok == false {
+				command = false
+				break
 			}
-			command = false
-			break
 		}
 
 		if command {
-			comment = strings.Replace(comment, "h", "4", -1)
-			comment = strings.Replace(comment, "j", "2", -1)
-			comment = strings.Replace(comment, "k", "8", -1)
-			comment = strings.Replace(comment, "l", "6", -1)
-			comment = strings.Replace(comment, " ", "5", -1)
+			for key, val := range mapper {
+				comment = strings.Replace(comment, string(key), string(val), -1)
+			}
 		}
 
 		fmt.Println("send:" + comment)
